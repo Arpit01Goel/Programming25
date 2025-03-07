a6ec9b5117a336ff61be245f7d447f7e2da87e41
Arpit <Arpit01goel@gmail.com>
2025-03-07 16:33:53 +0530

    Initial commit

---
+++ codeforces/temp.cpp
@@ -0,0 +1,28 @@
+/**
+ * @file temp.cpp
+ * @author Arpit Goel
+ * @date 2025/03/07 10:05:07
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+typedef long long ll;
+
+void solve() {
+    // Your solution code here
+    int n;
+    cin >> n;
+    cout << n << endl;
+}
+
+int main() { 
+    fast_io
+    int t;
+    cin >> t;
+    cout << t << endl;
+    return 0;
+}
+
+// GitHub: https://github.com/Arpit01Goel
